74886fab8ace78d0a3e32434a3959faf2884f3b6
AlaorFurquim <alaorfurquim34@gmail.com>
2023-11-19 21:52:56 -0300

    adicionando o som dos clicks ao clicar nas teclas da calculadora

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,5 +1,7 @@
 class CalcController {
     constructor() {
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
@@ -23,6 +25,16 @@ class CalcController {
         this.setLastNumberToDisplay();
         this.pasteFromClipBoard();
 
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+            btn.addEventListener('dblclick',e=>{
+                this.toggleAudio();
+            })
+        })
+
+    }
+
+    toggleAudio(){
+        this._audioOnOff = !this._audioOnOff;
     }
 
     pasteFromClipBoard(){
@@ -30,6 +42,13 @@ class CalcController {
             let Text = e.clipboardData.getData('Text');
             this.displayCalc = parseFloat(Text);
         })
+    }
+
+    playAudio(){
+        if(this._audioOnOff){
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
     }
 
     copyToClipBoard(){
@@ -48,7 +67,7 @@ class CalcController {
 
     initKeyBoard(){
         document.addEventListener('keyup', e=>{
-
+            this.playAudio();
             switch (e.key) {
                 case 'Escape':
                     this.clearAll();
@@ -251,6 +270,9 @@ class CalcController {
     }
 
     execBtn(value) {
+
+        this.playAudio();
+
         switch (value) {
             case 'ac':
                 this.clearAll();
